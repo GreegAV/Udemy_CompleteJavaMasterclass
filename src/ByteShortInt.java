7e62c57b17dcf1dbc2c9f4d06ec894b6861861be
GreegAV <greegav@gmail.com>
2017-08-05 23:39:57 +0300

    Lesson 4.19

--- src/ByteShortInt.java
+++ src/ByteShortInt.java
@@ -4,6 +4,7 @@
 public class ByteShortInt {
     public static void main(String[] args) {
         int myValue=1000;
-
+        int myMinValue = -2_147_483_648;
+        int myMaxValue = 2_147_483_647;
     }
 }
