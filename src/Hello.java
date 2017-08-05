cc71d4d7fc78378567165b42c7b0cd9d76ca3576
GreegAV <greegav@gmail.com>
2017-08-05 23:34:05 +0300

    Lesson 4.18

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+/**
+ * Created by avg-m on 05/08/2017.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        int myFirstNumber=(10+5)+(2*10);
+        int mySecondNumber=12;
+        int myThirdNumber=myFirstNumber*2;
+
+        int myTotal=myFirstNumber+mySecondNumber+myThirdNumber;
+
+        int myLastOne=1000-myTotal;
+
+        System.out.println(myLastOne);
+    }
+}
